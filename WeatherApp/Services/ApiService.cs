500697a4973204a5b1f9535b15872409d46f13c6
Student <Student@205-06>
2023-12-12 15:54:47 +0800

    加入專案檔案。

---
+++ WeatherApp/Services/ApiService.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public static class ApiService
+    {
+        public static async Task<Root> GetWeather(double latitude, double longitude)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&units=metric&appid=522a81b4d8344b0d02c7ae7d158604b8",latitude,longitude));
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+
+        public static async Task<Root> GetWeatherByCity(string city)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(string.Format("https://api.openweathermap.org/data/2.5/forecast?q={0}&units=metric&appid=522a81b4d8344b0d02c7ae7d158604b8",city));
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
